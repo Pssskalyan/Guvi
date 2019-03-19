c0df1749ff26d02bc0d15268eef23f38d2ba5374
PAVANKALYAN <41368953+Pssskalyan@users.noreply.github.com>
2019-03-19 16:38:23 +0530

    Create pavan.py

---
+++ pavan.py
@@ -0,0 +1,8 @@
+#To check weather a given number is positive,negitive or zero
+num=int(input("Enter a number:"))
+if num>0:
+print("Positive number")
+elif num<0:
+print("Negative number")
+else:
+print("Zero")
