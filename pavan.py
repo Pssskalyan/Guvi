e0bc76ce03eacdff3e32df6bd9cb5215392dc5fa
PAVANKALYAN <41368953+Pssskalyan@users.noreply.github.com>
2019-03-19 16:51:01 +0530

    Update pavan.py

--- pavan.py
+++ pavan.py
@@ -1,8 +1,8 @@
 #Checking weather given number is positive,negative or Zero
 num=float(input("Enter a number:"))
-if num>0:
-print("Positive number")
-elif num<0:
-print("Negative number")
-else:
-print("Zero")
+  if num>0:
+    print("Positive number")
+  elif num<0:
+    print("Negative number")
+  else:
+    print("Zero")
